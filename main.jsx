b38a02a8ac21ce16bf7fc152048d5930d4aba7cd
mauosuna98 <mauricioosuna1998@gmail.com>
2026-04-06 22:16:52 -0600

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Mbet from './Mbet.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Mbet />
-  </React.StrictMode>,
-)
